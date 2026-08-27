d8d570d818e2a903968f4977999078ed39a5ffa2
ParaswatiPutri <paraswati90965@gmail.com>
2026-08-27 16:28:28 +0700

    Add files via upload

---
+++ Proyek_1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prak_pbo0059;
+
+/**
+ *
+ * @author acer
+ */
+public class Proyek_1 {
+    public static void main(String[]args) {
+        System.out.println("Nama     : Nabilla Dwi Putri Paraswati");
+        System.out.println("NPM      : 2505060059");
+        System.out.println("Alamat   : Kota Magelang");
+        System.out.println("No_HP    : 123456789012");
+    }
+}
